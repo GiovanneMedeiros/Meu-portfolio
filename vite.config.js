ba14a93459cd14406c404d8a4aafe37288052ece
GiovanneDEV <giovannemedeiros22@gmail.com>
2026-03-14 08:33:42 -0300

    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/Meu-portfolio/",
+  base: "/portfolio/Meu-portfolio/",
   plugins: [react()],
   resolve: {
     alias: {
