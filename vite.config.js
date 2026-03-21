0d606b5980ac6058766a3cf907e252f01bfe7a73
GiovanneDEV <giovannemedeiros22@gmail.com>
2026-03-21 10:42:21 -0300

    fix: improve mobile responsiveness in projects section

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Meu-portfolio/",
+  
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  base: '/Meu-portfolio/',
+})
